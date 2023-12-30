f29bfbb3101a59370f8b8de0b3729d9ef914ebd3
gabrirm <gabrirm00@gmail.com>
2023-12-30 20:24:42 +0100

    first commit

---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg-red-500 h-screen flex flex-col">
+    <div
+      class="p-2 my-2 rounded-full hover:bg-blue-50 w-min dark:hover:bg-white/20"
+      :class="{ defaultTransition }"
+    >
+      <nuxt-link to="/">
+        <div class="w-8 h-8"><LogoTwitter /></div>
+      </nuxt-link>
+    </div>
+    <HomeIcon/>
+  </div>
+</template>
+
+<script setup>
+import {HomeIcon} from '@heroicons/vue/solid'
+const { defaultTransition } = useTailwindConfig();
+</script>
