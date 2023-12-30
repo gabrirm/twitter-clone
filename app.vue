6d2576d0c177600f86b5881b4bf4c8c589df80df
gabrirm <gabrirm00@gmail.com>
2023-12-30 21:21:53 +0100

    created grid cols structure

--- app.vue
+++ app.vue
@@ -6,14 +6,18 @@
           class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5"
         >
           <!-- Left sidebar -->
-          <div
-            class=" hidden md:block xs:col-span-1 xl:col-span-2"
-          >
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
             <div class="sticky top-0"><SidebarLeft /></div>
           </div>
           <!-- Main content -->
+          <main class="col-span-12 md:col-span-8 xl:col-span-6 bg-red-500">
+            <h1>asd</h1>
+          </main>
 
           <!-- Right sidebar -->
+          <div class="hidden md:block md:col-span-3 xl:col-span-4 bg-blue-500">
+            <div class="sticky top-0"><h1>rightsidebar</h1></div>
+          </div>
         </div>
       </div>
     </div>
---
+++ components/Sidebar/Left/Tab.vue
@@ -0,0 +1,25 @@
+<template>
+  <nuxt-link
+    to="#"
+    class="text-black transition ease-in-out duration-350 dark:hover:bg-dim-200 dark:text-white flex items-center p-3 w-min hover:bg-gray-200 rounded-full"
+  >
+    <div class="w-6 h-6 text-dark transition ease-in-out duration-300">
+      <slot name="icon" />
+    </div>
+    <div
+      class="ml-4 text-xl hidden xl:block"
+      :class="{ 'font-semibold': active, 'font-normal': !active }"
+    >
+      <slot name="name" />
+    </div>
+  </nuxt-link>
+</template>
+
+<script setup>
+const props = defineProps({
+  active: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
